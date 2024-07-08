bef75cb1b5e7f94b56974d21e1f34a4bf767128b
Shunquan Wang <shunquan.wang@ly.com>
2024-07-08 18:55:18 +0800

    feat: perfect pipe

--- typings.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@ramda/pipewith' {
-  export default function (
-    composer: (fn: Function, res: unknown) => unknown,
-    fns: Array<Function>
-  ): (...args: unknown[]) => unknown;
-}
